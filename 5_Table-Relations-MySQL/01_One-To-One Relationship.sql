a66a3b81e8a8e842d60ad5a3360ac328f7ea4a98
Deyan <dublinstand@gmail.com>
2017-04-12 14:01:52 +0100

    new file:   5_Table-Relations-MySQL/01_One-To-One Relationship.sql 	new file:   5_Table-Relations-MySQL/02_One-To-Many Relationship.sql

---
+++ 5_Table-Relations-MySQL/01_One-To-One Relationship.sql
@@ -0,0 +1,46 @@
+-- Create two tables as follows. Use appropriate data types.
+-- persons
+-- person_id	first_name	salary	passport_id
+-- 1  	Roberto                                            	43300.00	102
+-- 2	Tom	56100.00	103
+-- 3	Yana	60200.00	101
+-- passports
+-- passport	passport_number
+-- 101	N34FG21B
+-- 102	K65LO4R7
+-- 103	ZE657QP2
+-- Insert the data from the example above.
+-- Alter table persons and make person_id a primary key. Create a foreign key between persons and passports by using passport_id column.
+
+CREATE DATABASE test;
+
+USE test;
+
+SET @@auto_increment_increment=1;
+SET @@auto_increment_offset=1;
+
+CREATE TABLE persons(
+person_id INT NOT NULL,
+first_name VARCHAR(50) NOT NULL,
+salary FLOAT NOT NULL,
+passport_id INT
+);
+
+CREATE TABLE passports(
+passport INT NOT NULL PRIMARY KEY,
+passport_number VARCHAR(50) NOT NULL,
+UNIQUE KEY uk_passport_number(passport_number)
+);
+
+INSERT INTO persons(person_id, first_name, salary, passport_id)
+VALUES (1, 'Roberto', 43300.00, 102), (2, 'Tom', 56100.00, 103), (3, 'Yana', 60200.00, 101);
+
+INSERT INTO passports(passport, passport_number)
+VALUES (101, 'N34FG21B'), (102, 'K65LO4R7'),(103, 'ZE657QP2');
+
+ALTER TABLE persons
+ADD PRIMARY KEY pk_persons_id (person_id);
+
+ALTER TABLE persons
+ADD CONSTRAINT fk_persons_passports FOREIGN KEY (passport_id) REFERENCES passports(passport);
+
---
+++ 5_Table-Relations-MySQL/02_One-To-Many Relationship.sql
@@ -0,0 +1,20 @@
+-- Create two tables as follows. Use appropriate data types.
+-- manufacturers
+-- manufacturer_id	name	established_on
+-- 1  	BMW                                            	07/03/1916
+-- 2	Tesla	01/01/2003
+-- 3	Lada	01/05/1966
+-- models
+-- model_id	name	manufacturer_id
+-- 101	X1	1
+-- 102	i6	1
+-- 103	Model S	2
+-- 104	Model X	2
+-- 105	Model 3	2
+-- 106	Nova	3
+-- 
+-- 
+-- Insert the data from the example above. Add primary keys and foreign keys.
+
+USE test;
+
